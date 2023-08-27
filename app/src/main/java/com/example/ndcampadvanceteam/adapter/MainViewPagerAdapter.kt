0cfa587a53c6ff933601757fa85d8be0ccf69797
Jjol0twi <97600168+Jjol0twi@users.noreply.github.com>
2023-08-27 18:32:15 +0900

    add tabLayout ViewList

--- app/src/main/java/com/example/ndcampadvanceteam/adapter/MainViewPagerAdapter.kt
+++ app/src/main/java/com/example/ndcampadvanceteam/adapter/MainViewPagerAdapter.kt
@@ -5,12 +5,24 @@ import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.ndcampadvanceteam.BookmarkFragment
+import com.example.ndcampadvanceteam.R
 import com.example.ndcampadvanceteam.TodoFragment
 import com.example.ndcampadvanceteam.model.MainTabsModel
 
 //class MainViewPagerAdapter(tabList : ArrayList<MainTabsModel>, fragmentManager: FragmentManager, lifecycle: Lifecycle) :
 class MainViewPagerAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) :
     FragmentStateAdapter(fragmentManager, lifecycle) {
+
+    private val viewList = ArrayList<MainTabsModel>()
+    init {
+        viewList.add(MainTabsModel(TodoFragment.newInstance(), R.string.main_todo_title))
+        viewList.add(MainTabsModel(BookmarkFragment.newInstance(), R.string.main_bookmark_title))
+    }
+
+    fun getTitleByIndex(position : Int): Int {
+        return viewList[position].title
+    }
+
     override fun getItemCount(): Int = 2
 
     override fun createFragment(position: Int): Fragment {
