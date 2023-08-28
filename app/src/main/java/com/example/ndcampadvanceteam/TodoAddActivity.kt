5f1769b4d8b628e628b40a282416d3235925e218
Jjol0twi <97600168+Jjol0twi@users.noreply.github.com>
2023-08-28 10:44:38 +0900

    add binding&set toolbar

--- app/src/main/java/com/example/ndcampadvanceteam/TodoAddActivity.kt
+++ app/src/main/java/com/example/ndcampadvanceteam/TodoAddActivity.kt
@@ -1,11 +1,37 @@
 package com.example.ndcampadvanceteam
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import com.example.ndcampadvanceteam.databinding.TodoAddActivityBinding
 
 class TodoAddActivity : AppCompatActivity() {
+
+    private lateinit var binding: TodoAddActivityBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.todo_add_activity)
+        binding = TodoAddActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setToolbar()
+    private fun setToolbar() {
+        setSupportActionBar(binding.todoAddToolbar)
+        supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setDisplayShowTitleEnabled(true)
+            title = getString(R.string.todo_add_activity_title)
+        }
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
 }
